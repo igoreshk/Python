f0aec2ca5c38a3f05cf0a6c4acf7fc5e33d47bd4
Igor <80395407+igoreshk@users.noreply.github.com>
2024-11-21 19:33:00 +0300

    Update 19_string_aa_to_a2.py
    Ambigous var = 'l' rapleced to 'k'

--- 19_string_aa_to_a2.py
+++ 19_string_aa_to_a2.py
@@ -13,19 +13,19 @@ s = 'aaaabbсaa' преобразуется в 'a4b2с1a2', то есть
 
 # put your python code here
 s = str(input())
-l = len(s) - 1
+k = len(s) - 1
 c = 1
 t = ''
 if len(s) == 1:
     t = t + s + str(c)
 else:
-    for i in range(0,l):
-        if s[i] == s[i+1]:
+    for i in range(0, k):
+        if s[i] == s[i + 1]:
             c += 1
-        elif s[i] != s[i+1]:
+        elif s[i] != s[i + 1]:
             t = t + s[i] + str(c)
             c = 1
-    for j in range(l,l+1):
+    for j in range(k, k + 1):
         if s[-1] == s[-2]:
             t = t + s[j] + str(c)
         elif s[-1] != s[-2]:
